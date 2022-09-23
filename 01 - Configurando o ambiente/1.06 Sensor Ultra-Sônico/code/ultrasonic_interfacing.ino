76981fc12105824c2506c5b5ff4a95a0128557c8
Luís Henrique Bastos Magalhães da Cunha <magalhaes4010cms@gmail.com>
2022-09-23 17:05:07 -0300

    Update ultrasonic_interfacing.ino

--- 01 - Configurando o ambiente/1.06 Sensor Ultra-Sônico/code/ultrasonic_interfacing.ino
+++ 01 - Configurando o ambiente/1.06 Sensor Ultra-Sônico/code/ultrasonic_interfacing.ino
@@ -1,26 +1,14 @@
-#include <ros.h>
-#include <std_msgs/Empty.h>
-#include <std_msgs/Int16.h>
-
-ros::NodeHandle  nh;
-
-std_msgs::Int16 ultrasonic_msg
-ros::Publisher ultrasonic_node("ultrasonic_values", &ultrasonic_msg );
-
 // defines pins numbers
 const int trigPin = 2;
 const int echoPin = 4;
 // defines variables
 long duration;
 int distance;
-
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  nh.initNode();
-  nh.advertise(ultrasonic_node);
+  Serial.begin(9600); // Starts the serial communication
 }
-
 void loop() {
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
@@ -30,13 +18,10 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  
   duration = pulseIn(echoPin, HIGH);
   // Calculating the distance
   distance = duration * 0.034 / 2;
   // Prints the distance on the Serial Monitor
-  ultrasonic_msg.data = distance;
-  ultrasonic_node.publish(&ultrasonic_msg);
-  nh.spinOnce();
-  delay(1);
+  Serial.print("Distance: ");
+  Serial.println(distance);
 }
